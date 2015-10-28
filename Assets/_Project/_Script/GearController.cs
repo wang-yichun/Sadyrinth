88063f3d9d7dd86753c2ff42ed1670a903e2b3bf
Ethan <yishionwang314@163.com>
2015-10-28 18:51:57 +0800

    起落架OK
    Signed-off-by: Ethan <yishionwang314@163.com>

--- Assets/_Project/_Script/GearController.cs
+++ Assets/_Project/_Script/GearController.cs
@@ -1,16 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class GearController : MonoBehaviour {
 
+	public float ActionTime;
 	public Vector3 DownPosition;
 	public Vector3 UpPosition;
 
 	// 放起落架
 	public void GearDown() {
+		transform.DOLocalMove (DownPosition, ActionTime);
 	}
 
 	// 收起落架
 	public void GearUp() {
+		transform.DOLocalMove (UpPosition, ActionTime);
 	}
 }
