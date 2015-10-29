fef379828d36289b988dc62a2f2e91c9ed7672ba
Ethan <yishionwang314@163.com>
2015-10-29 12:14:12 +0800

    飞船火焰
    Signed-off-by: Ethan <yishionwang314@163.com>

---
+++ Assets/_Project/_Script/EngineController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineController : MonoBehaviour
+{
+
+	public ParticleSystem Smoke1;
+	public ParticleSystem Smoke2;
+
+	public ParticleSystem MainFire;
+
+	public float PowerToSmokeRate;
+	public float PowerToMainFireRate;
+
+	public void SetPower (float power)
+	{
+		Smoke1.emissionRate = power * PowerToSmokeRate;
+		Smoke2.emissionRate = power * PowerToSmokeRate;
+
+		Color color = MainFire.startColor;
+		MainFire.startColor = new Color (color.r, color.g, color.b, power * PowerToMainFireRate);
+	}
+}
--- Assets/_Project/_Script/PlayerController.cs
+++ Assets/_Project/_Script/PlayerController.cs
@@ -3,17 +3,21 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-	public Rigidbody rigidBody;
+	private Rigidbody rigidBody;
 	public Vector2 inputVec;
 
 	public float AutoTorqueRate;
+
+	public EngineController EngineBottom;
+	public EngineController EngineLeft;
+	public EngineController EngineRight;
 
 	public void InputVector (Vector2 vec)
 	{
 		inputVec = vec;
 	}
 
-	public static float InputToForceRate = .1f;
+	public static float InputToForceRate = .4f;
 
 	public static Vector3 InputToForce (Vector2 inputVec)
 	{
@@ -24,6 +28,57 @@ public class PlayerController : MonoBehaviour
 		);
 	}
 
+	public static float EngineMaximumForceLimit_Horizontal = 10f;
+	public static float EngineMaximumForceLimit_vertical = 30f;
+
+	public static Vector3 EngineMaximumForceLimitFilter (Vector3 inputForce)
+	{
+		Vector3 outputForce = new Vector3 (inputForce.x, inputForce.y, inputForce.z);
+
+		if (outputForce.y < 0) {
+			outputForce.y = 0;
+		}
+
+		if (outputForce.y > EngineMaximumForceLimit_vertical) {
+			outputForce.y = EngineMaximumForceLimit_vertical;
+		}
+
+		if (outputForce.x > EngineMaximumForceLimit_Horizontal) {
+			outputForce.x = EngineMaximumForceLimit_Horizontal;
+		}
+
+		if (outputForce.x < -EngineMaximumForceLimit_Horizontal) {
+			outputForce.x = -EngineMaximumForceLimit_Horizontal;
+		}
+
+		return outputForce;
+	}
+
+	public void AutoTorque ()
+	{
+		if (transform.up.x < 0) {
+			rigidBody.AddRelativeTorque (new Vector3 (0f, 0f, -AutoTorqueRate));	
+		} else {
+			rigidBody.AddRelativeTorque (new Vector3 (0f, 0f, AutoTorqueRate));	
+		}
+	}
+
+	public void SetEnginePower (Vector3 force)
+	{
+		EngineBottom.SetPower (force.y);
+
+		if (force.x > .1f) {
+			EngineLeft.SetPower (force.x);
+			EngineRight.SetPower (0);
+		} else if (force.x < .1f) {
+			EngineRight.SetPower (-force.x);
+			EngineLeft.SetPower (0);
+		} else {
+			EngineRight.SetPower (0);
+			EngineLeft.SetPower (0);
+		}
+	}
+
 	void Start ()
 	{
 		rigidBody = GetComponent<Rigidbody> ();
@@ -32,12 +87,13 @@ public class PlayerController : MonoBehaviour
 	void FixedUpdate ()
 	{
 		Vector3 force = InputToForce (inputVec);
+
+		force = EngineMaximumForceLimitFilter (force);
+
 		rigidBody.AddForce (force, ForceMode.Force);
 
-		if (transform.up.x < 0) {
-			rigidBody.AddRelativeTorque (new Vector3 (0f, 0f, -AutoTorqueRate));	
-		} else {
-			rigidBody.AddRelativeTorque (new Vector3 (0f, 0f, AutoTorqueRate));	
-		}
+		SetEnginePower (force);
+
+		AutoTorque ();
 	}
 }
