3b47631a91c963c9c6aec02d9c977fcfeb2725aa
Ethan <yishionwang314@163.com>
2015-10-30 17:27:08 +0800

    镜头跟随
    Signed-off-by: Ethan <yishionwang314@163.com>

---
+++ Assets/_Project/_Script/MainCameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraController : MonoBehaviour
+{
+	public PlayerController player;
+	public float LerpRate;
+	public Vector3 CameraOriginZeroPoint;
+
+	void LateUpdate ()
+	{
+		SetCameraPosition ();
+	}
+
+	void SetCameraPosition ()
+	{
+		Vector3 playerCameraSurfPoint = new Vector3 (player.transform.position.x, player.transform.position.y, CameraOriginZeroPoint.z);
+		transform.position = Vector3.Lerp (playerCameraSurfPoint, CameraOriginZeroPoint, LerpRate);
+	}
+}
