d4754d3798b813d7d38955ad4ae65df27bfefe69
Ethan <yishionwang314@163.com>
2015-10-31 12:02:40 +0800

    delete some comment
    Signed-off-by: Ethan <yishionwang314@163.com>

--- Assets/_Project/_Script/PlayerController.cs
+++ Assets/_Project/_Script/PlayerController.cs
@@ -145,17 +145,8 @@ public class PlayerController : MonoBehaviour
 
 	void OnCollisionEnter (Collision collision)
 	{
-
-		Debug.Log ("OnCollisionEnter");
-
 		foreach (ContactPoint contact in collision.contacts) {
-//			Debug.DrawRay (contact.point, contact.normal, Color.white);
-
 			GameObject collisionFX = Instantiate (CollisionFXPrefab, contact.point, Quaternion.identity) as GameObject;
 		}
-
-		//		if (collision.relativeVelocity.magnitude > 2)
-		//			audio.Play(); 
-
 	}
 }
