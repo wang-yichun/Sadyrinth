569772ee25836dce7bd325ce78f1d772da5f1da6
Ethan <yishionwang314@163.com>
2015-10-31 16:27:58 +0800

    prefab生成的地形,需要recenter - ferr2d
    Signed-off-by: Ethan <yishionwang314@163.com>

---
+++ Assets/_Project/_Script/EnvironmentController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironmentController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		ReCenterTerrian ();
+	}
+
+	void ReCenterTerrian ()
+	{
+		Ferr2D_Path[] list = GetComponentsInChildren<Ferr2D_Path> ();
+		foreach (Ferr2D_Path item in list) {
+			item.ReCenter ();
+		}
+	}
+}
